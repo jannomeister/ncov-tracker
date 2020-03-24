71ef775873113842b1d5e6ae36e3da0608a6e2b2
Janno Tabamo <janno@hyperstacksinc.com>
2020-03-24 16:14:21 +0800

    feat(dashboard): added initial dashboard component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Dashboard/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Dashboard
   }
 }
 </script>
@@ -21,8 +20,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  props: {}
+}
+</script>
+
+<style scoped>
+
+</style>
