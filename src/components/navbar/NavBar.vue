ad5d64b3582b1f217e01d4ad6466b5facc27d6b3
Janno Tabamo <janno@hyperstacksinc.com>
2020-03-31 22:40:25 +0800

    refactor(settings): transferred to navbar

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -8,17 +8,31 @@
       <v-spacer></v-spacer>
 
       <v-menu
-        v-model="menu"
+        v-model="changelog"
         :close-on-content-click="false"
         offset-y
       >
         <template v-slot:activator="{ on }">
-          <v-btn icon class="mr-3" v-on="on">
+          <v-btn icon class="mr-4" v-on="on">
             <v-icon x-large color="pink lighten-1">mdi-gift-outline</v-icon>
           </v-btn>
         </template>
 
         <Changelog />
+      </v-menu>
+
+      <v-menu
+        v-model="settings"
+        :close-on-content-click="false"
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon class="mr-4" v-on="on">
+            <v-icon x-large color="grey lighten-1">mdi-cog</v-icon>
+          </v-btn>
+        </template>
+
+        <Settings />
       </v-menu>
     </v-toolbar>
   </v-card>
@@ -26,13 +40,17 @@
 
 <script>
 import Changelog from './Changelog';
+import Settings from './Settings';
+
 export default {
   name: 'NavBar',
   components: {
     Changelog,
+    Settings,
   },
   data: () => ({
-    menu: false,
+    changelog: false,
+    settings: false,
   }),
 }
 </script>
--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -1,19 +1,16 @@
 <template>
   <v-container>
     <Urgent />
-    <Settings />
   </v-container>
 </template>
 
 <script>
 import Urgent from "./Urgent";
-import Settings from "./Settings";
 
 export default {
   name: 'SideBar',
   components: {
     Urgent,
-    Settings,
   }
 }
 </script>
